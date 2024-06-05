09fe3e919b2d5dcfa5ec438a92ea4df1e242ef0f
thayson <thayson@thayson-pc.intelbras.local>
2024-06-04 22:53:22 -0300

    service init

---
+++ services/conection/src/test/java/com/services/CoffeeStore/conection/ConectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.services.CoffeeStore.conection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ services/image/src/test/java/com/services/CoffeeStore/image/ImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.services.CoffeeStore.image;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
